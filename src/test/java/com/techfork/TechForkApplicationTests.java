844f23da41c7712242d4adc3a236e87db75c6f56
Dimo-2562 <chosehoon2@naver.com>
2025-10-05 21:55:17 +0900

    feat: 스프링 프로젝트 초기 셋팅

---
+++ src/test/java/com/techfork/TechForkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechForkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
