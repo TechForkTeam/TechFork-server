3cef9645adc98e89fa429d890d092189cfcf5208
Dimo-2562 <chosehoon2@naver.com>
2025-10-08 02:41:29 +0900

    fix: outputs.tf에서 value 변수 사용하도록 오류 해결

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,22 +1,22 @@
 output "ec2_public_ip" {
   description = "EC2 퍼블릭 IP"
-  content       = aws_instance.app.public_ip
+  value       = aws_instance.app.public_ip
 }
 
 output "ec2_public_dns" {
   description = "EC2 퍼블릭 DNS"
-  content       = aws_instance.app.public_dns
+  value       = aws_instance.app.public_dns
 }
 
 output "rds_endpoint" {
   description = "RDS 엔드포인트"
-  content       = aws_db_instance.main.endpoint
+  value       = aws_db_instance.main.endpoint
 }
 
 output "connection_info" {
   description = "연결 정보"
   sensitive   = true
-  content = <<-EOT
+  value = <<-EOT
     
     ===== Tech Blog 인프라 정보 =====
     
