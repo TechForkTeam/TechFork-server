e5ce6342f7a66e78270f2b95fb1f899125ce88d4
Dimo-2562 <chosehoon2@naver.com>
2025-10-08 04:52:37 +0900

    fix: 애플리케이션 설정 테스트 제거

--- src/test/java/com/techfork/TechForkApplicationTests.java
+++ src/test/java/com/techfork/TechForkApplicationTests.java
@@ -1,13 +1,8 @@
 package com.techfork;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TechForkApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
