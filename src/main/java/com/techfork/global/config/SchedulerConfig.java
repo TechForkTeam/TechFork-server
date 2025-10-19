ac733f0463accefdb6f8d37c95b7fd78ab37b191
Dimo-2562 <chosehoon2@naver.com>
2025-10-19 16:25:02 +0900

    chore: Redis 및 스케쥴링 설정 파일 구현

---
+++ src/main/java/com/techfork/global/config/SchedulerConfig.java
@@ -0,0 +1,12 @@
+package com.techfork.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 스케줄러 설정
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
