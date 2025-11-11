c71d85e4b6d2906f6eee6fdc4902781a9b2598fe
Dimo-2562 <chosehoon2@naver.com>
2025-11-12 02:47:24 +0900

    improve: 기존 메타데이터 추출 로직 제거

--- src/main/java/com/techfork/domain/post/repository/PostKeywordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techfork.domain.post.repository;
-
-import com.techfork.domain.post.entity.PostKeyword;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostKeywordRepository extends JpaRepository<PostKeyword, Long> {
-}
