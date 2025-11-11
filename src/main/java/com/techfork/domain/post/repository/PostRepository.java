77aaed29a2fcb5e12b51a174f79bcdb2f6aefa3d
Dimo-2562 <chosehoon2@naver.com>
2025-11-12 04:56:36 +0900

    feat: PostSummaryReader 구현

--- src/main/java/com/techfork/domain/post/repository/PostRepository.java
+++ src/main/java/com/techfork/domain/post/repository/PostRepository.java
@@ -3,7 +3,11 @@ package com.techfork.domain.post.repository;
 import com.techfork.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     boolean existsByUrl(String url);
+
+    List<Post> findBySummaryIsNull();
 }
