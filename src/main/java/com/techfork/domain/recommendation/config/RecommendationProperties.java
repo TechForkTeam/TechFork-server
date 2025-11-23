c9b8d0bfcdf4015d61d1316dcd33cf4792872bbd
Dimo-2562 <chosehoon2@naver.com>
2025-11-23 17:15:46 +0900

    feat: 설정 파일 구현

---
+++ src/main/java/com/techfork/domain/recommendation/config/RecommendationProperties.java
@@ -0,0 +1,52 @@
+package com.techfork.domain.recommendation.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "recommendation")
+public class RecommendationProperties {
+
+    private Integer knnSearchSize = 100;
+
+    private Integer numCandidates = 200;
+
+    private Integer mmrFinalSize = 30;
+
+    private Double lambda = 0.6;
+
+    private Integer activeUserHours = 24;
+
+    private EmbeddingWeights embeddingWeights = new EmbeddingWeights();
+
+    private TimeDecay timeDecay = new TimeDecay();
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class EmbeddingWeights {
+        private Double title = 0.4;
+        private Double summary = 0.4;
+        private Double content = 0.2;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TimeDecay {
+        private Double days7 = 1.3;
+        private Double days30 = 1.0;
+        private Double days90 = 0.7;
+        private Double daysOver = 0.4;
+    }
+}
