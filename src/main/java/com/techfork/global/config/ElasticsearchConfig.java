b282a7c7ae0bde565cb2f8ec343e867957b25145
Dmori <83327857+Dimo-2562@users.noreply.github.com>
2025-11-26 02:33:23 +0900

    fix: ES 리포지토리 설정 어노테이션 제거 (#111)

--- src/main/java/com/techfork/global/config/ElasticsearchConfig.java
+++
@@ -1,9 +0,0 @@
-package com.techfork.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.config.EnableElasticsearchAuditing;
-
-@Configuration
-@EnableElasticsearchAuditing
-public class ElasticsearchConfig {
-}
