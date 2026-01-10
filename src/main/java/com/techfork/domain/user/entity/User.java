d325a9058f8c7126c8f8b744f9468592a8ae47b6
dmori <chosehoon2@naver.com>
2026-01-10 17:08:29 +0900

    feat: 유저 엔티티 필드 추가

--- src/main/java/com/techfork/domain/user/entity/User.java
+++ src/main/java/com/techfork/domain/user/entity/User.java
@@ -1,13 +1,8 @@
 package com.techfork.domain.user.entity;
 
 import com.techfork.global.common.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +13,13 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class User extends BaseTimeEntity {
+
+    private String nickName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String description;
 
     @OneToMany
     @JoinColumn(name = "user_id")
