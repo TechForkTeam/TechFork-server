aa321547bbd62e31bb6f8da8b5a2bbc61ebce41b
dmori <chosehoon2@naver.com>
2026-01-12 12:51:47 +0900

    chore: 사용하지 않는 import문 제거

--- src/main/java/com/techfork/domain/post/dto/CompanyDto.java
+++ src/main/java/com/techfork/domain/post/dto/CompanyDto.java
@@ -3,8 +3,6 @@ package com.techfork.domain.post.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
-
 @Builder
 @Schema(name = "CompanyDto", description = "회사 정보")
 public record CompanyDto(
