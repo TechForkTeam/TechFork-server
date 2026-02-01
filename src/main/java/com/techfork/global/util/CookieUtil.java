3bb3ad9bec1cc64c4da36cf377e6c5f110506146
dmori <chosehoon2@naver.com>
2026-02-01 20:44:34 +0900

    fix: 쿠키에 SameSite 옵션을 None으로 하여 CORS 에러 해결

--- src/main/java/com/techfork/global/util/CookieUtil.java
+++ src/main/java/com/techfork/global/util/CookieUtil.java
@@ -3,29 +3,34 @@ package com.techfork.global.util;
 import com.techfork.global.constant.Constants;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 
 public final class CookieUtil {
     private CookieUtil() {}
 
     public static void addRefreshTokenCookie(HttpServletResponse response, String domain, String token, long maxAge) {
-        Cookie cookie = new Cookie(Constants.REFRESH_TOKEN_COOKIE_NAME, token);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setDomain(domain);
-        cookie.setMaxAge((int) (maxAge / 1000)); // milliseconds to seconds
+        ResponseCookie cookie = ResponseCookie.from(Constants.REFRESH_TOKEN_COOKIE_NAME, token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .domain(domain)
+                .maxAge(maxAge / 1000)
+                .sameSite("None")
+                .build();
 
-        response.addCookie(cookie);
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 
     public static void deleteRefreshTokenCookie(HttpServletResponse response, String domain) {
-        Cookie cookie = new Cookie(Constants.REFRESH_TOKEN_COOKIE_NAME, null);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setDomain(domain);
-        cookie.setMaxAge(0); // 즉시 만료
+        ResponseCookie cookie = ResponseCookie.from(Constants.REFRESH_TOKEN_COOKIE_NAME, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .domain(domain)
+                .maxAge(0)
+                .sameSite("None")
+                .build();
 
-        response.addCookie(cookie);
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 }
